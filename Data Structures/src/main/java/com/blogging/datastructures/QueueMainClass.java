45279723b15edb76486c2cde3bb605b52ae3f2eb
Sandeep Dass <sandeepdass003@gmail.com>
2020-08-27 09:47:31 +0530

    Queue implementation using java.util.LinkedList<T>

---
+++ Data Structures/src/main/java/com/blogging/datastructures/QueueMainClass.java
@@ -0,0 +1,25 @@
+package com.blogging.datastructures;
+
+public class QueueMainClass {
+
+	public static void main(String[] args) {
+		QueueMainClass obj = new QueueMainClass();
+		obj.test();
+	}
+
+	private void test() {
+		Queue<String> queue = new Queue<String>();
+		queue.enqueue("Sandeep");
+		queue.enqueue("Dass");
+		queue.enqueue("Aman");
+		queue.enqueue("Kaur");
+		queue.enqueue("Ojus");
+		queue.enqueue("Nikita");
+		queue.dequeue();
+		queue.dequeue();
+		for(int offset = 0; offset < queue.getSize(); offset++) {
+			System.out.println(queue.getItem(offset));
+		}
+	}
+
+}
