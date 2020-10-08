6b4a56ac80e0511876009b532402b5f467002606
Sandeep Dass <sandeepdass003@gmail.com>
2020-10-08 19:37:57 +0530

    Selection Sort Program

---
+++ Data Structures/src/main/java/com/blogging/datastructures/sorting/SelectionSort.java
@@ -0,0 +1,33 @@
+package com.blogging.datastructures.sorting;
+
+public class SelectionSort {
+
+	public static void main(String[] args) {
+		int[] data = new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+		
+		System.out.println("Input Data:-");
+		for(int val : data) {
+			System.out.println(val);
+		}
+		
+		for(int sortedIndex = 0; sortedIndex < data.length; sortedIndex++) {
+			int smallestNumber = data[sortedIndex];
+			int swapTargetIndex = sortedIndex;
+			for(int position = sortedIndex; position < data.length; position++) {
+				if(data[position] < smallestNumber) {
+					smallestNumber = data[position];
+					swapTargetIndex = position;
+				}
+			}
+			int temp = data[swapTargetIndex];
+			data[swapTargetIndex] = data[sortedIndex];
+			data[sortedIndex] = temp;
+		}
+		
+		System.out.println("Sorted Data:-");
+		for(int val : data) {
+			System.out.println(val);
+		}
+	}
+
+}
