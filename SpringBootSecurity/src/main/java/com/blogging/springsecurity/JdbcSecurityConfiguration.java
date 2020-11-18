888fcc88c645e04f124f89db04347344aa0eb560
Sandeep Dass <sandeepdass003@gmail.com>
2020-11-18 21:38:44 +0530

    Spring Boot Security authorization using JDBC - H2

--- SpringBootSecurity/src/main/java/com/blogging/springsecurity/JdbcSecurityConfiguration.java
+++ SpringBootSecurity/src/main/java/com/blogging/springsecurity/JdbcSecurityConfiguration.java
@@ -1,26 +1,27 @@
 package com.blogging.springsecurity;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
-public class SecutiryConfiguration extends WebSecurityConfigurerAdapter {
+public class JdbcSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private DataSource dataSource;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-		.withUser("Admin")
-		.password("Admin")
-		.roles("ADMIN")
-		.and()
-		.withUser("User")
-		.password("User")
-		.roles("USER");
+		auth.jdbcAuthentication()
+			.dataSource(dataSource);
 	}
 	
 	@Bean
