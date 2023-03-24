eae1283b52cf975de49b67638fd25790e13510fe
Sandeep Dass <sandeepdass003@gmail.com>
2023-03-24 17:39:11 -0400

    Using junit4

--- JavaTesting/src/test/java/com/blogging/MainTest.java
+++ JavaTesting/src/test/java/com/blogging/MainTest.java
@@ -1,8 +1,8 @@
 package com.blogging;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
 public class MainTest {
 
